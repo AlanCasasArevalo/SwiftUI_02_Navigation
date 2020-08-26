3b93cc41089e030c93232aaa35e1f1685c93b3c8
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-08-26 10:06:26 +0200

    [FEATURE][NAVIGATION BUTTON] * New navigation button was created to navigate from main view to second view

--- SwiftUI_02_Navigation/ContentView.swift
+++ SwiftUI_02_Navigation/ContentView.swift
@@ -12,8 +12,28 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             NavigationLink(destination: SecondView()) {
-                Text("Segunda vista")
+                HStack{
+                    Image(systemName: "hand.point.right.fill")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    .shadow(color: .black, radius: 10, x: 2, y: 2)
+                    Text("Ir a segunda vista")
+                        .foregroundColor(.white)
+                        .shadow(color: .black, radius: 10, x: 2, y: 2)
+                }
+                .padding()
+                .background(
+                    LinearGradient(gradient: Gradient(colors: [.gray, .blue]), startPoint: .trailing, endPoint: .leading)
+                )
+                .cornerRadius(10, antialiased: true)
+                .shadow(color: .gray, radius: 10, x: 10, y: 15)
             }
+                //Clasico
+                //.navigationBarTitle("Principal", displayMode: .inline)
+                // Mas nuevo
+                //.navigationBarTitle("Principal", displayMode: .large)
+                // Automatico que es el diseño nuevo
+                .navigationBarTitle("Principal", displayMode: .automatic)
         }
     }
 }
